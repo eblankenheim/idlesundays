27f4bc27c553d7e3294c2088ab7bde3af4894241
eblankenheim <eblankenheim@simplisoda.com>
2026-05-19 21:55:56 -0500

    add trailingSlash:true to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   // Static export for S3 deployment
   // The build output will be in ./out/ directory
   images: {
